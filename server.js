4acb4dc10aa1965f5ceccbb9ec770dc9b63716d8
CJ237 <79546904+CJ237@users.noreply.github.com>
2024-08-30 21:52:13 -0500

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 const PORT = process.env.port || 3001;
-const { readFromFile, readAndAppend } = require('/public/assets/helpers/fsUtils.js');
+const { readFromFile, readAndAppend } = require('./public/assets/helpers/fsUtils.js');
 
 const app = express();
 
